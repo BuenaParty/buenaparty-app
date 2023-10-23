bbffe14750a61422d5481300cc8b670e865209e3
Vinicius <viniciussoaresribeiro773@gmail.com>
2023-10-23 07:21:52 -0300

    Rotas modulares deletadas

--- buena-party-app/routes/user.js
+++
@@ -1,9 +0,0 @@
-const serverExpress = require('../server');
-
-const testUser = console.log('O roteamento está funcionando!');
-
-serverExpress.get('/user/test', _ => {
-    console.log('Está funcionando!')
-});
-
-module.exports = serverExpress;
